56567dfdd49fadd8f0e28346473cc530407051e2
Mark Nottingham <mnot@mnot.net>
2011-07-14 16:47:22 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Sweet: Setting HTTP Content-Disposition Headers
+
+Getting the syntax for HTTP Content-Disposition headers to work well with browsers well is tricky, especially if you want the filename to contain non-ASCII characters. 
+
+Sweet is here to help. 
+
+It exposes one function - make\_disposition - that returns a value
+suitable for use as a Content-Disposition header value.
+
+For example:
+
+  var http = require('http');
+  var sweet = require('sweet');
+
+  # "my stuff" in Japanese
+  var cd_value = sweet.make_disposition(
+    'attachment', '私のもの', 'my stuff'
+  );
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Content-Disposition': cd_value;
+    });
+    res.end('Hello World\n');
+  }).listen(1337, "127.0.0.1");
+
+
+See [RFC6266](http://tools.ietf.org/html/rfc6266) for the spec and related details.
+
+Note that Sweet is *just* for setting Content-Disposition in HTTP responses (e.g., for file downloads); it won't help with MIME multi-part messages, for example.
+
+
+## Contact
+
+Mark Nottingham <mnot@mnot.net>
+
+https://github.com/mnot/sweet
---
+++ test/test.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var sweet = require('../lib/index');
+var assert = require('assert');
+
+var test_cases = [
+  {
+    disposition: 'attachment',
+    filename: 'foo.html',
+    filename_fallback: 'foo.html',
+    expected: 'attachment; filename="foo.html"'
+  },
+  {
+    disposition: 'attachment',
+    filename: 'foo.html',
+    expected: 'attachment; filename="foo.html"'
+  },
+  {
+    disposition: 'attachment',
+    filename: 'foo bar.html',
+    expected: 'attachment; filename="foo bar.html"'
+  },
+  {
+    disposition: 'attachment',
+    filename: 'foo "bar".html',
+    expected: 'attachment; filename="foo \\"bar\\".html"'
+  },
+  {
+    disposition: 'attachment',
+    filename: 'föö.html',
+    expected: null
+  },
+  {
+    disposition: 'attachment',
+    filename: 'föö.html',
+    filename_fallback: 'foo.html',
+    expected: 'attachment; filename="foo.html"; filename*=utf-8\'\'f%C3%B6%C3%B6.html'
+  }
+];
+
+for (test_num in test_cases) {
+  var test_case = test_cases[test_num];
+  var generated = sweet.make_disposition(
+    test_case.disposition,
+    test_case.filename,
+    test_case.filename_fallback
+  );
+  assert.equal(generated, test_case.expected, 
+    "failed " + test_num + " (" + generated + ")"
+  );
+}
